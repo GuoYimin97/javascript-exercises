79539f0629191cf32dbfa60c1593f3ee0f8ab16f
Yimin Guo <81876378+GuoYimin97@users.noreply.github.com>
2024-11-06 12:05:45 -0600

    Complete exercise02 of js-exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let mystr = "";
+    if (num < 0) {
+        mystr = "ERROR";
+    } else {
+        for (i = 0; i < num; i++) {
+            mystr = mystr + string;
+        }
+    }
+    return mystr;
 };
 
 // Do not edit below this line
