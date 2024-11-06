bc88bb7acffab9785194a8b59537da31bc129630
Yimin Guo <81876378+GuoYimin97@users.noreply.github.com>
2024-11-06 12:22:01 -0600

    Complete exercise03 of js-exercises

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(inputStr) {
+    let outputStr = "";
+    for (i = inputStr.length-1; i > -1; i--) {
+        outputStr += inputStr[i];
+    }
+    return outputStr;
 };
+
+// console.log(reverseString("DonaldJTrump"))
 
 // Do not edit below this line
 module.exports = reverseString;
