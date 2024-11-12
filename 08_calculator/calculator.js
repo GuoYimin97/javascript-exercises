278f70e28b3059e4a64f33d013656e39978d3989
Yimin Guo <81876378+GuoYimin97@users.noreply.github.com>
2024-11-12 09:44:13 -0600

    Complete exer08 of the js-exercises

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,46 @@
-const add = function() {
-	// this is a test
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+  return arr.reduce((total, currentItem) => { return total + currentItem; }, 0);
 };
 
-const multiply = function() {
+// console.log(sum([2, 4, 6, 8, 10]))
 
+const multiply = function (arr) {
+  return arr.reduce((total, currentItem) => { return total * currentItem; }, 1);
 };
 
-const power = function() {
-	
+// console.log(multiply([2, 4, 6, 8, 10, 12, 14]))
+
+const power = function (a, b) {
+  let result = 1;
+  for (i = 1; i < b + 1; i++) {
+    result = result * a;
+  }
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function (a) {
+  if (a > 0 && typeof a === "number"
+    && Number.isInteger(a)
+  ) {
+    let arr = [];
+    for (i = 0; i < a; i++) {
+      arr[i] = a - i;
+    }
+    return arr.reduce((total, currentItem) => { return total * currentItem; }, 1);
+  } else if (a === 0) {
+    return 1;
+  } else {
+    return "ERROR";
+  }
+
 };
 
 // Do not edit below this line
