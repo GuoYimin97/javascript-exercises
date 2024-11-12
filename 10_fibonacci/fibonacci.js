7abfe8de9e4b792a9bcc40a57994a026f6cb71f4
Yimin Guo <81876378+GuoYimin97@users.noreply.github.com>
2024-11-12 11:00:02 -0600

    Complete exer10 thru 12 of js-exers

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,28 @@
-const fibonacci = function() {
+const fibonacci = function (inputNumber) {
+
+    if (typeof inputNumber !== "number") {
+        n = Number(inputNumber);
+    } else {
+        n = inputNumber;
+    }
+
+    let arr = [1, 1, 2, 3, 5, 8];
+    if (n < 0) {
+        return "OOPS";
+    } else if (n === 0) {
+        return 0;
+    } else if (n <= 6) {
+        return arr[n - 1];
+    } else {
+        for (i = 6; i < n; i++) {
+            arr[i] = arr[i - 1] + arr[i - 2];
+        }
+        return arr[n - 1];
+    }
 
 };
+
+// console.log(fibonacci(-25));
 
 // Do not edit below this line
 module.exports = fibonacci;
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,37 @@
-const findTheOldest = function() {
+const findTheOldest = function (inputArr) {
+    function findTheOldest_ForReduce(cumulative, currentItem) {
+        if (currentItem.yearOfDeath === undefined) {
+            currentItem.yearOfDeath = new Date().getFullYear();
+        }
+        if (cumulative.yearOfDeath - cumulative.yearOfBirth < currentItem.yearOfDeath - currentItem.yearOfBirth) {
+            cumulative = currentItem;
+        }
+        return cumulative;
+    }
 
+    return inputArr.reduce(findTheOldest_ForReduce, 
+        {name: "Default",
+        yearOfBirth: new Date().getFullYear(),
+        yearOfDeath: new Date().getFullYear()});
 };
+
+// const people = [
+//     {
+//         name: "Carly",
+//         yearOfBirth: 2018,
+//     },
+//     {
+//         name: "Jane",
+//         yearOfBirth: 1912,
+//         yearOfDeath: 1941,
+//     },
+//     {
+//         name: "Ray",
+//         yearOfBirth: 1962,
+//         yearOfDeath: 2011,
+//     },
+// ];
+// console.log(findTheOldest(people).name)
 
 // Do not edit below this line
 module.exports = findTheOldest;
